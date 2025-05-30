db77ba8a47f69e772949a8c3356d69635ddb26a2
Fabianzkp <fabianzkp@gmail.com>
2025-05-30 16:31:07 +0100

    wk3 nextjs tutorial

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -8,10 +8,16 @@ export const metadata: Metadata = {
   title: 'Edit Invoice',
 };
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id;
-  const customers = await fetchCustomers();
-  const invoice = await fetchInvoiceById(id);
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params; // Await the params Promise
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
 
   if (!invoice) {
     notFound();
